975502db744a281e8364cb4f9ead20603d325cd3
Gustavo Gomes <68404385+gustavoldndev@users.noreply.github.com>
2020-11-16 13:09:03 -0300

    Add files via upload

---
+++ Js/Font.js
@@ -0,0 +1,14 @@
+window.FontAwesomeCdnConfig = {
+    autoA11y: {
+      enabled: false
+    },
+    asyncLoading: {
+      enabled: false
+    },
+    reporting: {
+      enabled: false
+    },
+    useUrl: "use.fontawesome.com",
+    faCdnUrl: "https://cdn.fontawesome.com:443",
+    code: "3a8d059f5c",
+    
